6aba51b30c6d818d705d18bb8832c7222716567d
candacecodes <candacehchoe@gmail.com>
2020-09-28 15:20:59 -0700

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,14 @@ user1 = User.create(username: "username1")
 deck1 = Deck.create(description: "description1", user_id: 1)
 
 card1 = Card.create(description: "card description 1", imgsrc: "imgsrc1", deck_id: 1)
+card2 = Card.create(description: "card description 2", imgsrc: "imgsrc2", deck_id: 1)
+card3 = Card.create(description: "card description 3", imgsrc: "imgsrc3", deck_id: 1)
+card4 = Card.create(description: "card description 4", imgsrc: "imgsrc4", deck_id: 1)
+card5 = Card.create(description: "card description 5", imgsrc: "imgsrc5", deck_id: 1)
+card6 = Card.create(description: "card description 6", imgsrc: "imgsrc6", deck_id: 1)
+card7 = Card.create(description: "card description 7", imgsrc: "imgsrc7", deck_id: 1)
+card8 = Card.create(description: "card description 8", imgsrc: "imgsrc8", deck_id: 1)
+card9 = Card.create(description: "card description 9", imgsrc: "imgsrc9", deck_id: 1)
+card10 = Card.create(description: "card description 10", imgsrc: "imgsrc10", deck_id: 1)
 
 puts 'seeds done'
