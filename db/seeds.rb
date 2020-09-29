963a74f1e0a04a0ac476ae2cae2dc02caa1880ff
candacecodes <candacehchoe@gmail.com>
2020-09-29 16:30:12 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,13 @@ deck5 = Deck.create(description: "deck description5", user_id: 4)
 
 card1 = Card.create(description: "card description 1", imgsrc: "imgsrc1", deck_id: 1)
 card2 = Card.create(description: "card description 2", imgsrc: "imgsrc2", deck_id: 1)
-card3 = Card.create(description: "card description 3", imgsrc: "imgsrc3", deck_id: 1)
-card4 = Card.create(description: "card description 4", imgsrc: "imgsrc4", deck_id: 1)
-card5 = Card.create(description: "card description 5", imgsrc: "imgsrc5", deck_id: 1)
-card6 = Card.create(description: "card description 6", imgsrc: "imgsrc6", deck_id: 1)
-card7 = Card.create(description: "card description 7", imgsrc: "imgsrc7", deck_id: 1)
-card8 = Card.create(description: "card description 8", imgsrc: "imgsrc8", deck_id: 1)
-card9 = Card.create(description: "card description 9", imgsrc: "imgsrc9", deck_id: 1)
+card3 = Card.create(description: "card description 3", imgsrc: "imgsrc3", deck_id: 2)
+card4 = Card.create(description: "card description 4", imgsrc: "imgsrc4", deck_id: 2)
+card5 = Card.create(description: "card description 5", imgsrc: "imgsrc5", deck_id: 3)
+card6 = Card.create(description: "card description 6", imgsrc: "imgsrc6", deck_id: 3)
+card7 = Card.create(description: "card description 7", imgsrc: "imgsrc7", deck_id: 4)
+card8 = Card.create(description: "card description 8", imgsrc: "imgsrc8", deck_id: 4)
+card9 = Card.create(description: "card description 9", imgsrc: "imgsrc9", deck_id: 5)
 
 
 puts 'seeds done'
